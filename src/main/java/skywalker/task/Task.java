4df4ffe63bdb5729664ab073ec2b075febe8f076
dominic2412 <521hxt@gmail.com>
2024-08-29 18:15:27 +0800

    Revert "Complete Java Doc"
    This reverts commit d902f8e1122c8b8ad447ebefd786742bab8b4b81.

--- src/main/java/skywalker/task/Task.java
+++ src/main/java/skywalker/task/Task.java
@@ -6,30 +6,16 @@ public class Task {
     public boolean isDone;
     protected TaskType taskType;
 
-    /**
-     * Constructs a Task with the specified description and task type.
-     * The task is initialised to be undone.
-     * @param description
-     * @param taskType
-     */
     public Task(String description, TaskType taskType) {
         this.description = description;
         this.isDone = false;
         this.taskType = taskType;
     }
 
-    /**
-     * Returns a 'X' if the task is done and empty space otherwise.
-     * @return "X" if the task is done, otherwise a space character " "
-     */
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
 
-    /**
-     * Mark the task as done if undone
-     * Else print a statement to say that it is alr done.
-     */
     public void markDone(){
         if(!this.isDone) {
             this.isDone = true;
@@ -38,10 +24,6 @@ public class Task {
         }
     }
 
-    /**
-     * Mark the task as undone if done
-     * Else print a statement to say that it is alr undone.
-     */
     public void unmarkDone() {
         if(this.isDone) {
             this.isDone = false;
@@ -50,12 +32,6 @@ public class Task {
         }
     }
 
-    /**
-     * Returns a string representation of the task, including its status icon
-     * and description.
-     *
-     * @return A string representation of the task if the "[X] description"
-     */
     @Override
     public String toString(){
         return "[" + getStatusIcon()+ "] " + description;
